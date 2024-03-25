e422a3c6aae73c67b56ddecea3d2ce150e715440
EkrashZahid123 <f2022065090@umt.edu.pk>
2024-03-25 01:23:22 -0700

    Basic of typescript in code

---
+++ Assignment/PIAIC.js
@@ -0,0 +1,17 @@
+"use strict";
+// Declare a variable for storing your favorite movie name with a string type annotation.
+let movie = "Sher Dil";
+console.log("The Name of the movie is as:", movie);
+// Create a template string to introduce yourself, including your name and age.
+let name1 = "Ekrash Zahid";
+let age = 19;
+let introduction = `My name is ${name1} and My age is ${age}`;
+console.log(introduction);
+//Initialize a constant with a type annotation for your current mood as a boolean
+const current_mood = true;
+console.log("My Current mood is Happy", current_mood);
+//Combine a user's first and last name using template literals and log the full name.
+let firstName = "Ekrash";
+let lastName = "Zahid";
+let full = `My Full Name is as:${firstName}  ${lastName}`;
+console.log(full);
