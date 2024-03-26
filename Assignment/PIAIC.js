205f7e64cfdb81a84b4622d6ed42b09873ef5605
EkrashZahid123 <f2022065090@umt.edu.pk>
2024-03-26 10:09:47 -0700

    Assignment Complete Helpful in understanding typescript fundamentals

--- Assignment/PIAIC.js
+++ Assignment/PIAIC.js
@@ -23,3 +23,29 @@ let product_name = "DELL i5 8 generation";
 let Price_of_product = 1200000;
 let merg = `The Name of product is ${product_name} and price is ${Price_of_product}`;
 console.log(merg);
+//Declare a variable to store the number of pets you have with a number type annotation.
+let No_of_Pet = 21;
+console.log("The total number of pets :", No_of_Pet);
+//Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let birthday = true;
+console.log("Today is my birthday", birthday);
+//Create a template string that includes a user's first name, last name, and age with proper labels.
+let First_Name = "Ekrash";
+let Last_Name = "Zahid";
+let Age = 19;
+let template_string = `My name is ${First_Name} ${Last_Name} and My age is ${Age} `;
+console.log(template_string);
+//Initialize a constant to hold your birth year with a number type annotation.
+const birth_year = 2003;
+console.log("My Birthday Year is", birth_year);
+/*Write a line of code that uses a template string to combine
+a city and country into a full address.*/
+let city_name = "Rahim Yar Khan";
+let country_name = "Pakistan";
+let Address = "Johar Town C1 Block Near UMT";
+let merge1 = `My City Name is ${city_name} ,My country Name is ${country_name},and My address is ${Address}`;
+console.log(merge1);
+/*Declare a variable for today's temperature with a number type annotation
+ and assign a value to it*/
+let today_temp = 25;
+console.log("The Today Temp is ", today_temp, "Degree");
--- Assignment/PIAIC.ts
+++ Assignment/PIAIC.ts
@@ -22,3 +22,30 @@ let product_name: string = "DELL i5 8 generation";
 let Price_of_product: number = 1200000;
 let merg = `The Name of product is ${product_name} and price is ${Price_of_product}`;
 console.log(merg);
+//Declare a variable to store the number of pets you have with a number type annotation.
+let No_of_Pet: number = 21;
+console.log("The total number of pets :", No_of_Pet);
+//Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let birthday: boolean = true;
+console.log("Today is my birthday", birthday);
+//Create a template string that includes a user's first name, last name, and age with proper labels.
+let First_Name: string = "Ekrash";
+let Last_Name: string = "Zahid";
+let Age: number = 19;
+let template_string: string = `My name is ${First_Name} ${Last_Name} and My age is ${Age} `;
+console.log(template_string);
+//Initialize a constant to hold your birth year with a number type annotation.
+const birth_year:number=2003;
+console.log("My Birthday Year is",birth_year);
+/*Write a line of code that uses a template string to combine 
+a city and country into a full address.*/
+let city_name:string="Rahim Yar Khan";
+let country_name:string="Pakistan";
+let Address:string="Johar Town C1 Block Near UMT";
+let merge1:string=`My City Name is ${city_name} ,My country Name is ${country_name},and My address is ${Address}`;
+console.log(merge1);
+/*Declare a variable for today's temperature with a number type annotation
+ and assign a value to it*/
+ let today_temp:number=25;
+ console.log("The Today Temp is ",today_temp,"Degree");
+ 
